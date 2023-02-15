7834d6f02d74a372fa5c00ffabfb2f5c01203e68
xtredme <eaglet.hoagie_0h@icloud.com>
2023-02-15 15:44:17 +0300

    Добавили занимающегося спортом человека

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,16 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var score: Int = 1
+    
+    private var runMan: Bool = false
+    
     @IBOutlet weak var labelValue: UILabel!
     
     @IBOutlet weak var buttonTap: UIButton!
     
-    private var score: Int = 1
+    @IBOutlet weak var imageRunner: UIImageView!
     
+    @IBOutlet weak var buttonDidNull: UIButton!
+    
+    @IBAction func buttonDidNullTap(_ sender: Any) {
+        score = 1
+        labelValue.text = "«Значение счётчика: 0»"
+    }
     @IBAction func buttonDid(_ sender: Any) {
         labelValue.text = "«Значение счётчика: \(score)»"
         score += 1
+        
+        if runMan {
+            imageRunner.image = UIImage(systemName: "figure.flexibility")
+        } else {
+            imageRunner.image = UIImage(systemName: "figure.strengthtraining.functional")
+        }
+        runMan.toggle()
     }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         labelValue.text = "«Значение счётчика: 0»"
