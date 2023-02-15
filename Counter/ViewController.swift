fc439d72647f281ca54170b2d7c9d72e7136565f
xtredme <eaglet.hoagie_0h@icloud.com>
2023-02-15 15:27:16 +0300

    добавили кнопку обнуление и ImageView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelValue.text = "«Значение счётчика: 0»"
         // Do any additional setup after loading the view.
     }
 
